5a170b213a32f8343fc245ac3b6979c5f218a0d8
jrenvois <jrenvois@student.42.fr>
2025-05-18 00:50:34 +0200

    remove: all

--- src/db/__init__.py
+++
@@ -1,6 +0,0 @@
-from .models import Base, User, Auction, Order, OrderStatus, OrderType
-from .database import engine, SessionLocal, get_db
-from .crud import DatabaseManager
-
-# Créer les tables au démarrage
-Base.metadata.create_all(bind=engine)
